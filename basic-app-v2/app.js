f99db8250727be46fffdf72ef44b55bbe3e51039
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-12 09:58:45 +0200

    Note added

--- basic-app-v2/app.js
+++ basic-app-v2/app.js
@@ -1,20 +1,21 @@
-Vue.component('the-button', {
+// NOTE: Vue 2!
+Vue.component("the-button", {
   template: '<button @click="updateMessage">Click me</button>',
   methods: {
     updateMessage() {
-      this.$emit('update');
+      this.$emit("update");
     },
   },
 });
 
 new Vue({
-  el: '#app',
+  el: "#app",
   data: {
-    message: 'This works in Vue 2!',
+    message: "This works in Vue 2!",
   },
   methods: {
     changeMessage() {
-      this.message = 'Will it work in Vue 3?';
+      this.message = "Will it work in Vue 3?";
     },
   },
 });
